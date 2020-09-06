ae2d2cafed1480b8ba36c17f2b4d7989a698389a
CHRIST003 <christnganga06@gmail.com>
2020-09-06 13:25:21 +0200

    get the latest pull request

--- src/test/java/com/startup/service/user/impl/UserServiceImplTest.java
+++ src/test/java/com/startup/service/user/impl/UserServiceImplTest.java
@@ -6,6 +6,7 @@ package com.startup.service.user.impl;
 import com.startup.entity.user.User;
 import com.startup.factory.user.UserFactory;
 import com.startup.service.user.UserService;
+import org.junit.Assert;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
@@ -22,21 +23,21 @@ public class UserServiceImplTest {
     @Test
     public void d_getAll() {
         Set<User> users = service.getAll();
-        assertEquals(1,users.size());
+        Assert.assertEquals(1,users.size());
         System.out.println(service.getAll());
     }
 
     @Test
     public void a_create() {
         User created = service.create(user);
-        assertEquals(user.getUserId(),created.getUserId());
+        Assert.assertEquals(user.getUserId(),created.getUserId());
         System.out.println("create " + created);
     }
 
     @Test
     public void b_read() {
         User read = service.read(user.getUserId());
-        assertEquals(user.getUserId(),read.getUserId());
+        Assert.assertEquals(user.getUserId(),read.getUserId());
         System.out.println("Read " + read);
     }
 
@@ -44,15 +45,15 @@ public class UserServiceImplTest {
     public void c_update() {
         User updated = new User.Builder().copy(user).lastname("Ngwenya").build();
         updated = service.update(updated);
-        assertEquals(user.getUserId(),updated.getUserId());
-        assertNotEquals(user.getLastname(),updated.getLastname());
+        Assert.assertEquals(user.getUserId(),updated.getUserId());
+        Assert.assertNotEquals(user.getLastname(),updated.getLastname());
         System.out.println("update: " + updated);
     }
 
     @Test
     public void e_delete() {
         boolean deleted = service.delete(user.getUserId());
-        assertTrue(deleted);
+        Assert.assertTrue(deleted);
         System.out.println("deleted :" + deleted);
     }
 
