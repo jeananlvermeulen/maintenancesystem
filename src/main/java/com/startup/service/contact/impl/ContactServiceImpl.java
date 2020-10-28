e8bbf8061d80b3ca891aefacc48b55f6928dc336
Harbinger0 <shabied007@gmail.com>
2020-10-28 04:38:48 +0200

    commit

--- src/main/java/com/startup/service/contact/impl/ContactServiceImpl.java
+++ src/main/java/com/startup/service/contact/impl/ContactServiceImpl.java
@@ -2,52 +2,52 @@ package com.startup.service.contact.impl;
 /**
  * @author @CloeteSC
  * Desc: Test Repository for Contact
- * Date: 05 September 2020
+ * Date: 27 October 2020
  */
 import com.startup.entity.contact.Contact;
 import com.startup.repository.contact.ContactRepository;
-import com.startup.repository.contact.impl.ContactRepositoryImpl;
 import com.startup.service.contact.ContactService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
+import java.util.stream.Collectors;
+
 @Service
 public class ContactServiceImpl implements ContactService {
 
     private static ContactService service = null;
+    @Autowired
     private ContactRepository repository;
-
-    private ContactServiceImpl(){
-        this.repository = ContactRepositoryImpl.getRepository();
-    }
-
-    public static ContactService getService(){
-        if (service == null) service = new ContactServiceImpl();
-        return service;
-    }
 
     @Override
     public Set<Contact> getAll() {
-        return this.repository.getAll();
+        return this.repository.findAll().stream().collect(Collectors.toSet());
     }
 
     @Override
     public Contact create(Contact contact) {
-        return this.repository.create(contact);
+        return this.repository.save(contact);
     }
 
     @Override
     public Contact read(String s) {
-        return this.repository.read(s);
+        return this.repository.findById(s).orElseGet(null);
     }
 
     @Override
     public Contact update(Contact contact) {
-        return this.repository.update(contact);
+        if (this.repository.existsById(contact.getPhone())){
+            return this.repository.save(contact);
+        }
+        return null;
+
     }
 
     @Override
     public boolean delete(String s) {
-        return this.repository.delete(s);
+        this.repository.deleteById(s);
+        if(this.repository.existsById(s)) return false;
+        else return true;
     }
 }
